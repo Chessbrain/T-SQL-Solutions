8f449b010df32332c550adad18a39ad15336ac05
Chessbrain <chessbrain94@gmail.com>
2019-07-10 17:11:20 +0200

    BULK TSV Insert

---
+++ File Imports/TSV.sql
@@ -0,0 +1,10 @@
+-- This query uses 0x0a for ROWTERMINATOR because TSVs sometimes give unintended results when using \t due to arrays.
+-- If arrays exist in the source data, keep in mind this query does not split the arrays into separate rows on input.
+
+BULK INSERT T
+FROM 'C:\Users\Chessbrain\Desktop\Database Folder\IMDB DB DUMP\title.crew.tsv\data.tsv'
+WITH (
+	FIRSTROW = 2, -- Exclude this if your data does not have a header.
+	ROWTERMINATOR = '0x0a'
+)
+GO
